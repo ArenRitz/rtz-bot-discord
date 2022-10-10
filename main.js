e7a6d07bea8164e18a04e9f1de210a07b5b7413d
ArenRitz <aren.dolbakian@gmail.com>
2022-10-09 23:22:34 -0400

    Add test slash commands

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
 const Discord = require("discord.js")
 const { Client, GatewayIntentBits } = require('discord.js');
-const test = require('dotenv').config();
+require('dotenv').config();
 
-console.log(process.env.TOKEN_DISCORD);
+const token = process.env.TOKEN;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 // when the bot starts up
@@ -23,7 +23,20 @@ client.on("messageCreate", message => {
   }
 })
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
 // Add bot token in the "quotes" below
-client.login(process.env.TOKEN_DISCORD)
+client.login(token)
 
 
