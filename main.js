5f3067f7366609daabaf9080d8967e45f14a9b60
ArenRitz <aren.dolbakian@gmail.com>
2022-10-14 14:34:20 -0400

    Use SlashcommandBuilder

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ import { config } from "dotenv";
 import { Client, GatewayIntentBits } from "discord.js";
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord.js";
+import { SlashCommandBuilder } from "@discordjs/builders";
+
 config();
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const GUILD_ID = process.env.GUILD_ID;
 const TOKEN = process.env.TOKEN;
-
 
 const client = new Client({
   intents: [
@@ -20,54 +21,82 @@ const client = new Client({
 
 const rest = new REST({ version: "10" }).setToken(TOKEN);
 
-
-
-
 // when the bot starts up
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
 client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isCommand()) return;
-    if (interaction.commandName === "ping") {
-        console.log(interaction.type);
-         
-        await interaction.reply({content: "Pong!"});
-    }
-
-
-
+  if (!interaction.isCommand()) return;
+  if (interaction.commandName === "order") {
+    await interaction.reply({
+      content: `You ordered ${interaction.options.getString(
+        "food"
+      )} and ${interaction.options.getString("drink")} to drink!`,
+    });
+  }
 });
 
-
-
-
 async function main() {
-    const commands = [{
-        name: "ping",
-        description: "Replies with Pong!",
-    }];
+  const orderCommand = new SlashCommandBuilder()
+    .setName("order")
+    .setDescription("Order food and drink")
+    .addStringOption((option) =>
+      option
+        .setName("food")
+        .setDescription("What food would you like?")
+        .setRequired(true)
+        .setChoices(
+          {
+            name: "Pizza",
+            value: "pizza",
+          },
+          {
+            name: "Pasta",
+            value: "pasta",
+          },
+          {
+            name: "Burger",
+            value: "burger",
+          }
+        )
+    )
+    .addStringOption((option) =>
+      option
+        .setName("drink")
+        .setDescription("What drink would you like?")
+        .setRequired(true)
+        .setChoices(
+          {
+            name: "Coke",
+            value: "coke",
+          },
+          {
+            name: "Pepsi",
+            value: "pepsi",
+          },
+          {
+            name: "Sprite",
+            value: "sprite",
+          }
+        )
+    );
 
-    try {
-        console.log("Started refreshing application (/) commands.");
-        await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands });
+  const commands = [orderCommand.toJSON()];
 
+  try {
+    console.log("Started refreshing application (/) commands.");
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+      body: commands,
+    });
 
-        client.login(TOKEN);
-    } catch (error) {
-
-        console.error(error);
-    }
+    client.login(TOKEN);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 main();
-
-
-
-
-
 
 // client.on("messageCreate", (message) => {
 //   //react to messages
@@ -83,5 +112,3 @@ main();
 //     message.react("👀");
 //   }
 // });
-
-
